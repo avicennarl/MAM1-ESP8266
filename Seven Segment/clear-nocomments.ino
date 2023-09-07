02fd61608f155ab13fd64a3d00a3dbc4087ad34e
avicennarl <92698716+avicennarl@users.noreply.github.com>
2023-09-08 04:30:58 +0700

    Update clear-nocomments.ino

--- Seven Segment/clear-nocomments.ino
+++ Seven Segment/clear-nocomments.ino
@@ -3,32 +3,27 @@
 const char* ssid = "MyESP8266Hotspot"; 
 const char* password = "Password123";   
 
-
 int a = D0;  
-int b = D1;  // Gpio-1 with b of 7 segment display    
-int c = D2;  // Gpio-2 with c of 7 segment display  
-int d = D3;  // Gpio-3 with d of 7 segment display  
-int e = D4;  // Gpio-4 with e of 7 segment display   
-int f = D5;  // Gpio-5 with f of 7 segment display  
-int g = D6;  // Gpio-6 with g of 7 segment display
-int dp = D7; // Gpio-7 with dp (decimal point) of 7 segment display
+int b = D1; 
+int c = D2;  
+int d = D3;   
+int e = D4;  
+int f = D5; 
+int g = D6; 
+int dp = D7; 
 
 WiFiServer server(80);
-
-bool dpState = false; // State to toggle DP on/off
+bool dpState = false; 
 
 void setup() {
   Serial.begin(115200);
   delay(10);
 
-  // Inisialisasi NodeMCU sebagai titik akses WiFi
   WiFi.softAP(ssid, password);
 
-  // IP address dari NodeMCU saat berada dalam mode titik akses
   IPAddress apIP(192, 168, 1, 1);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
 
-  // Declaring Seven segment pins as Output
   pinMode(a, OUTPUT);
   pinMode(b, OUTPUT);
   pinMode(c, OUTPUT);
@@ -36,9 +31,8 @@ void setup() {
   pinMode(e, OUTPUT);
   pinMode(f, OUTPUT);
   pinMode(g, OUTPUT);
-  pinMode(dp, OUTPUT); // Set DP pin as OUTPUT
-
-  // Set semua segmen Seven Segment menjadi non-aktif
+  pinMode(dp, OUTPUT); 
+  
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
   digitalWrite(c, HIGH);
@@ -46,89 +40,66 @@ void setup() {
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
-  digitalWrite(dp, HIGH); // Set DP pin to off
+  digitalWrite(dp, HIGH); 
 
-  // Start the server
+  
   server.begin();
   Serial.println("Server started");
 
-  // Print IP address hotspot di serial monitor
+  
   Serial.print("Hotspot IP Address: ");
   Serial.println(WiFi.softAPIP());
 
-  // Print SSID hotspot di serial monitor
+  
   Serial.print("Hotspot SSID: ");
   Serial.println(ssid);
 }
 
 void loop() {
-  // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
     return;
   }
-
-  // Wait until the client sends some data
   while (!client.available()) {
     delay(1);
   }
-
-  // Read the first line of the request
   String request = client.readStringUntil('\r');
-
   int value = 0;
-
   if (request.indexOf("/Req=1") != -1) {
-    // Displaying 1
     setSevenSegment(1);
     value = 1;
   } else if (request.indexOf("/Req=2") != -1) {
-    // Displaying 2
     setSevenSegment(2);
     value = 2;
   } else if (request.indexOf("/Req=3") != -1) {
-  // Displaying 3
   setSevenSegment(3);
   value = 3;
 } else if (request.indexOf("/Req=4") != -1) {
-  // Displaying 4
   setSevenSegment(4);
   value = 4;
 } else if (request.indexOf("/Req=5") != -1) {
-  // Displaying 5
   setSevenSegment(5);
   value = 5;
 } else if (request.indexOf("/Req=6") != -1) {
-  // Displaying 6
   setSevenSegment(6);
   value = 6;
 } else if (request.indexOf("/Req=7") != -1) {
-  // Displaying 7
   setSevenSegment(7);
   value = 7;
 } else if (request.indexOf("/Req=8") != -1) {
-  // Displaying 8
   setSevenSegment(8);
   value = 8;
 } else if (request.indexOf("/Req=9") != -1) {
-  // Displaying 9
   setSevenSegment(9);
   value = 9;
 }
-
-  // Check for DP toggle request
   if (request.indexOf("/DPToggle") != -1) {
-    // Toggle DP state
     dpState = !dpState;
-    digitalWrite(dp, dpState ? LOW : HIGH); // Set DP pin based on dpState
+    digitalWrite(dp, dpState ? LOW : HIGH);
   }
-
-  // Send the HTTP response
   sendHttpResponse(client, value);
 }
-
 void setSevenSegment(int number) {
-  // Set semua segmen menjadi non-aktif (HIGH)
   digitalWrite(a, HIGH);
   digitalWrite(b, HIGH);
   digitalWrite(c, HIGH);
@@ -136,15 +107,12 @@ void setSevenSegment(int number) {
   digitalWrite(e, HIGH);
   digitalWrite(f, HIGH);
   digitalWrite(g, HIGH);
-
   switch (number) {
     case 1:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 1
       digitalWrite(b, LOW);
       digitalWrite(c, LOW);
       break;
     case 2:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 2
       digitalWrite(a, LOW);
       digitalWrite(b, LOW);
       digitalWrite(g, LOW);
@@ -152,7 +120,6 @@ void setSevenSegment(int number) {
       digitalWrite(d, LOW);
       break;
     case 3:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 3
       digitalWrite(a, LOW);
       digitalWrite(b, LOW);
       digitalWrite(c, LOW);
@@ -160,14 +127,12 @@ void setSevenSegment(int number) {
       digitalWrite(g, LOW);
       break;
     case 4:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 4
       digitalWrite(b, LOW);
       digitalWrite(c, LOW);
       digitalWrite(f, LOW);
       digitalWrite(g, LOW);
       break;
     case 5:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 5
       digitalWrite(a, LOW);
       digitalWrite(c, LOW);
       digitalWrite(d, LOW);
@@ -175,7 +140,6 @@ void setSevenSegment(int number) {
       digitalWrite(g, LOW);
       break;
     case 6:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 6
       digitalWrite(a, LOW);
       digitalWrite(c, LOW);
       digitalWrite(d, LOW);
@@ -184,13 +148,11 @@ void setSevenSegment(int number) {
       digitalWrite(g, LOW);
       break;
     case 7:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 7
       digitalWrite(a, LOW);
       digitalWrite(b, LOW);
       digitalWrite(c, LOW);
       break;
     case 8:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 8
       digitalWrite(a, LOW);
       digitalWrite(b, LOW);
       digitalWrite(c, LOW);
@@ -200,7 +162,6 @@ void setSevenSegment(int number) {
       digitalWrite(g, LOW);
       break;
     case 9:
-      // Aktifkan segmen-segmen yang diperlukan untuk menampilkan 9
       digitalWrite(a, LOW);
       digitalWrite(b, LOW);
       digitalWrite(c, LOW);
@@ -210,7 +171,6 @@ void setSevenSegment(int number) {
       break;
   }
 }
-
 void sendHttpResponse(WiFiClient client, int value) {
   client.println("HTTP/1.1 200 OK");
   client.println("Content-Type: text/html");
@@ -232,7 +192,6 @@ void sendHttpResponse(WiFiClient client, int value) {
     client.print(i);
     client.println("</button></a></p>");
   }
-  // Add button to toggle DP
   client.print("<p align=center><a href=\"/DPToggle\"><button>Toggle DP</button></a></p>");
   client.println("</body>");
   client.println("</html>");
