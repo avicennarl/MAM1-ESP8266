6641e456e5d359d435f19e779799984b272c9bf5
avicennarl <92698716+avicennarl@users.noreply.github.com>
2023-09-10 20:45:49 +0700

    Update Versi1-MotorDC.cpp

--- Motor DC/Versi1-MotorDC.cpp
+++ Motor DC/Versi1-MotorDC.cpp
@@ -8,19 +8,13 @@ const char* password = "KataSandiHotspot";
 // Pin yang terhubung ke driver motor DC (L298N)
 const int motorPin1 = D1; // IN1 pada L298N
 const int motorPin2 = D2; // IN2 pada L298N
-const int enablePin = D3; // ENABLE pada L298N (modul PWM)
 
 ESP8266WebServer server(80);
-
-int motorSpeed = 0; // Variabel untuk menyimpan kecepatan motor (0 - 255)
 
 void setup() {
   // Inisialisasi motor pins sebagai output
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
-
-  // Inisialisasi pin ENABLE sebagai output
-  pinMode(enablePin, OUTPUT);
   
   // Mulai serial communication
   Serial.begin(115200);
@@ -55,29 +49,24 @@ void handleRoot() {
   html += "</head>";
   html += "<body>";
   html += "<h1>Kendalikan Motor DC</h1>";
-  html += "<label for='speedSlider'>Kecepatan:</label>";
-  html += "<input type='range' min='0' max='255' value='0' class='slider' id='speedSlider'>";
-  html += "<br>";
   html += "<button class='button' onclick='forward()'>Maju</button>";
   html += "<button class='button' onclick='reverse()'>Mundur</button>";
   html += "<button class='button' onclick='stopMotor()'>Berhenti</button>";
   html += "<script>";
-  html += "function sendCommand(command, speed) {";
+  html += "function sendCommand(command) {";
   html += "var xhr = new XMLHttpRequest();";
   html += "xhr.open('POST', '/control', true);";
   html += "xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');";
-  html += "xhr.send('command=' + command + '&speed=' + speed);";
+  html += "xhr.send('command=' + command);";
   html += "}";
   html += "function forward() {";
-  html += "var speed = document.getElementById('speedSlider').value;";
-  html += "sendCommand('forward', speed);";
+  html += "sendCommand('forward');";
   html += "}";
   html += "function reverse() {";
-  html += "var speed = document.getElementById('speedSlider').value;";
-  html += "sendCommand('reverse', speed);";
+  html += "sendCommand('reverse');";
   html += "}";
   html += "function stopMotor() {";
-  html += "sendCommand('stop', 0);";
+  html += "sendCommand('stop');";
   html += "}";
   html += "</script>";
   html += "</body>";
@@ -88,10 +77,6 @@ void handleRoot() {
 
 void handleControl() {
   String command = server.arg("command");
-  int speed = server.arg("speed").toInt();
-
-  // Mengatur kecepatan motor berdasarkan nilai speed
-  analogWrite(enablePin, speed);
 
   // Tambahkan logika untuk mengontrol motor DC berdasarkan perintah yang diterima
   if (command == "forward") {
